23fbb2569204a288fe8e419b858fc81f426215f3
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-03-10 15:10:51 +0800

    a little modification on structure

--- interface/operator-code in C++/geo.hpp
+++ interface/operator-code in C++/geo.hpp
@@ -4,3 +4,18 @@
   @date:   2018/03
 */
 
+// for a detector
+class Detector;
+class OddDetector;
+class DetectorPair;
+
+class Points; // a vecter of point.
+class Lines; // a vector of line.
+
+//get the position list of all the crystals in a detector.
+Points get_crystal_positions(const Detector& detector);
+
+Points get_crystal_positions(const OddDetector& detector);
+
+// get all the possible lines connect the Detectorpairs.
+Lines make_lors(const DetectorPair);
--- interface/structure/operator/eventprocessor.h
+++ interface/structure/operator/eventprocessor.h
@@ -9,9 +9,15 @@ typedef std::vector<Events> EventsGroup;
 
 class EventsProcessor{
 public:
-    virtual void wash(const Scanner& scanner, Events& events);
+    //the Events may record the interaction position of Gamma with the crystal,
+    //to maintain the consistence with realistic detecting process, the events are transform to the 
+    //virtual discrere crystal position.
+    virtual void transform_to_virtual_position(const Scanner& scanner, Events& events);
 
-    virtual EventsGroup partition(const Events& events);
-    virtual EventsGroup split(const Events& events, unsigned split_num);
-    virtual Events merge(EventsGroup);
+    //patition the events to several
+    virtual void partition(const Events& events, EventsGroup& events_group);
+
+    virtual void split(const Events& events, unsigned split_num, EventsGroup& events_group);
+    
+    virtual void merge(const EventsGroup& events_group, Events& events);
 }
