8e3b866235b0255c6d4ea6fdd2a574f2a1617cb0
twj2417 <twj2417@163.com>
2018-05-10 20:07:13 +0800

    update

--- src/python/srf/task/tasksino_info.py
+++ src/python/srf/task/tasksino_info.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta
 
-from .sino import SinoTask
+from .sinodatanew import SinoToRSpec, ReconSpec, MatrixToRSpec
 # from task import SRFTaskInfo
 
 class SRFTaskInfo(metaclass=ABCMeta):
@@ -18,7 +18,7 @@ class SRFTaskInfo(metaclass=ABCMeta):
 
 
 class SinoTaskInfo(SRFTaskInfo):
-    task_cls = SinoTask
+    #task_cls = SinoTask
     _fields = {
         'Recon_info',
         'Image_info',
@@ -29,3 +29,35 @@ class SinoTaskInfo(SRFTaskInfo):
         super().__init__(task_configs)
 
     
+class SRFTaskSpec:
+    # task_cls = None
+    task_type = None
+
+    def __init__(self, config):
+        self.work_directory = config['work_directory']
+
+    def to_dict(self):
+        return {'work_directory': self.work_directory}
+
+
+
+class SinoTaskSpec(SRFTaskSpec):
+    # task_cls = TorTask
+    task_type = 'SinoTask'
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.image = ImageSpec(config['image'])
+        self.sino = SinoToRSpec(config['sino'])
+        self.matrix = MatrixToRSpec(config['matrix'])
+        self.osem = OSEMSpec(config['osem'])
+
+    def to_dict(self):
+        result = super().to_dict()
+        result.update({
+            'image': self.image.to_dict(),
+            'sino': self.sino.to_dict(),
+            'matrix': self.matrix.to_dict(),
+            'osem': self.osem.to_dict(),
+        })
+        return result
