701f7dad21c9dfd3c5e0fbdcfed74d36674c80de
HongXiang <hx.hongxiang@gmail.com>
2018-05-29 09:11:47 +0800

    demos

---
+++ src/python/srf/tensor/image.py
@@ -0,0 +1,23 @@
+from dxl.learn import Tensor, Variable
+
+
+class Image(Tensor):
+    def split_xyz(self, graph):
+        pass
+
+
+class ImageVariable(Variable):
+    def as_tensor(self):
+        # No new tensorflow tensor is created, just for unified high order interface.
+        return Image()
+
+
+class ImageXYZ(Tensor):
+    def __init__(self, image: Image, is_roate):
+        pass
+
+    def rotation(self):
+        pass
+
+    def merge(self):
+        return Image()
