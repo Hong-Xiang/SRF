1c0202134dfab603c405e4564b0225eb2d166f5c
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-05-30 15:47:02 +0800

    test for effmap task

--- tests/python/scanner/test_scanner.py
+++ tests/python/scanner/test_scanner.py
@@ -1,11 +1,16 @@
 import unittest
 
-from srf.scanner import Scanner
+from srf.scanner.pet import PETScanner
 
 
-class TestScanner(unittest.TestCase):
-    def test_init(self):
-        scanner = Scanner()
-        
-        
+# class TestScanner(unittest.TestCase):
+#     def test_init(self):
+#         scanner = Scanner()
 
+
+#     def test_get_projection(self):
+#         scanner = Scanner()
+
+#         result_proj =  scanner.get_projection()
+
+#     def test
