a3401f656042a39fb0e12082aa54a5aa03789a0e
HongXiang <hx.hongxiang@gmail.com>
2018-06-04 18:51:56 +0800

    mark deps

---
+++ src/python/srf/data/loader.py
@@ -0,0 +1,13 @@
+from dxl.learn.core import ConfigurableWithName
+
+
+class MasterLoader:
+    pass
+
+
+class WorkerLoader:
+    pass
+
+
+class OSEMWorkerLoader:
+    pass
--- src/python/srf/graph/reconstruction/local_reconstruction.py
+++ src/python/srf/graph/reconstruction/local_reconstruction.py
@@ -1,10 +1,14 @@
 from dxl.learn.core import Graph
 
 
-class ReconstructionGraph(Graph):
-    def __init__(self, master_data_loader, worker_data_loader, )
-
-
 class LocalReconstructionGraph(Graph):
-    def __init__(self, master_data_loader, worker_data_loader, *, config):
+    def __init__(self, info, master_data_loader, worker_data_loader, *, config):
+        self._master_data_loader = master_data_loader
+        self._worker_data_loader = worker_data_loader
+        super().__init__(info, config=config)
+
+    def kernel(self):
+        pass
+
+    def run(self):
         pass
