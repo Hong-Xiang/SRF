d5f702b3b61e66df78a5dff9c877c8962258cda8
HongXiang <hx.hongxiang@gmail.com>
2018-06-05 08:56:36 +0800

    add example

---
+++ examples/ver0605/run.py
@@ -0,0 +1,36 @@
+from srf.graph.reconstruction.local_reconstruction import LocalReconstructionGraph
+from srf.data.loader import MasterLoader, WorkerLoader
+from dxl.learn.core import SubgraphMakerTable
+from srf.model.projection import ProjectionToR
+from srf.model.backprojection import BackProjectionToR
+
+from dxl.core.debug import enter_debug
+from dxl.learn.core.config import update_config
+from dxl.learn.core import Session
+
+enter_debug()
+
+
+def main():
+    SubgraphMakerTable.register(
+        'reconstruction/worker/reconstruction/projection', ProjectionToR)
+    SubgraphMakerTable.register(
+        'reconstruction/worker/reconstruction/backprojection', BackProjectionToR)
+    update_config('reconstruction/worker/reconstruction',
+                  {'center': [0.0, 0.0, 0.0]})
+    update_config('reconstruction/worker/reconstruction',
+                  {'size': [666.9, 666.9, 1422.72]})
+    update_config('projection_model', {
+        'tof_sigma2': 162.30,
+        'tof_bin': 6.0,
+        'kernel_width': 3.86,
+    })
+    ml = MasterLoader(shape=[195, 195, 416])
+    ll = WorkerLoader('lors_debug.npz', 'summap.npy')
+    g = LocalReconstructionGraph('reconstruction', ml, ll, nb_iteration=10)
+    sess = Session()
+    g.run(sess)
+
+
+if __name__ == "__main__":
+    main()
