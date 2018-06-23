9ede77da0d61118bb927a53f5888054349a9a8b1
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-06-23 18:07:06 +0800

    informal commit

---
+++ src/python/srf/scanner/pet/block.py
@@ -0,0 +1,70 @@
+
+import numpy as np
+
+from dxl.shape.rotation.matrix import *
+from dxl.shape.utils.vector import Vector3
+from dxl.shape.utils.axes import Axis3, AXIS3_X, AXIS3_Z
+
+from srf.scanner.geometry import Vec3
+
+
+class Block(object):
+    def get_meshes(self):
+        raise NotImplementedError
+
+
+class RingBlock(Block):
+    """
+    
+    """
+    def __init__(self, block_size: Vec3, center: Vec3, grid: Vec3,
+                 rad_z: np.float32):
+        self._block_size = block_size
+        self._center = center
+        self._grid = grid
+        self._rad_z = rad_z
+
+    @property
+    def grid(self):
+        return self._grid
+
+    @property
+    def center(self):
+        return self._center
+
+    @property
+    def rad_z(self):
+        return self._rad_z
+
+    @property
+    def block_size(self):
+        return self._block_size
+
+    def get_meshes(self) -> np.array:
+        """
+        return all of the crystal centers in a block
+        """
+
+        interval = self.block_size.value / self.grid.value
+        grid = self.grid.value
+
+        p_start = self.center.value - self.block_size.value + interval/2
+        p_end = self.center.value + self.block_size.value - interval/2
+
+        mrange = [np.linspace(p_start[i], p_end[i], grid[i]) for i in range(3)]
+
+        meshes = np.array(np.meshgrid(mrange[0], mrange[1], mrange[2]))
+
+        # print(meshes.shape)
+        meshes = np.transpose(meshes)
+        source_axis = AXIS3_X
+        target_axis = Axis3(
+            Vector3([np.cos(self.rad_z), np.sin(self.rad_z), 0]))
+        rot = axis_to_axis(source_axis, target_axis)
+
+        rps = rot @ np.reshape(meshes, (3, -1))
+        return np.transpose(rps)
+
+
+class PatchBlock(Block):
+    raise NotImplementedError
--- tests/python/scanner/test_block.py
+++ tests/python/scanner/test_block.py
@@ -1,7 +1,50 @@
-import unittest
+import numpy as np
+from srf.test import TestCase
+import pytest
 
-from srf.scanner.pet import Block
+from srf.scanner.pet import RingBlock, PatchBlock 
+from srf.scanner.geometry import Vec3 
+
+class BlockTestBase(TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def get_attrs(self):
+        grid = Vec3(3, 2, 1)
+        size = Vec3( 6.0, 2.0, 1.0)
+        center = Vec3(0.0 ,0.0 ,0.0)
+        rad_z = 90.0
+        return [grid, size, center, rad_z]
+    
+    def get_meshes(self):
+        pass
 
 
+class TestRingBlock(TestCase):
 
+    def make_RingBlock(self):
+        attrs = self.get_attrs()
+        grid = attrs[0]
+        size = attrs[1]
+        center = attrs[2]
+        rad_z = attrs[3]
+        return RingBlock(grid, size, center, rad_z)
+
+    def test_make_block(self):
+        b = self.make_RingBlock()
+        assert isinstance(b , RingBlock)
+    
+    def test_make_meshes(self):
+        b = self.make_RingBlock()
+        result_meshes = b.get_meshes()
+        expected_meshes = self.get_meshes()
+        self.assertFloatArrayEqual(result_meshes, expected_meshes)
+        
+
+class PatchTestBase(TestCase):
+    def setUp(self):
+        super.setUp()
+    
+    def get_attrs(self):
+        raise NotImplementedError
 
