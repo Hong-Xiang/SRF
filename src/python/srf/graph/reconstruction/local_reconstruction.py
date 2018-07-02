9d6b139f848177b13a4a11a8b7d792c5d63d1b32
HongXiang <hx.hongxiang@gmail.com>
2018-07-03 01:06:49 +0800

    recon runable

--- src/python/srf/graph/reconstruction/local_reconstruction.py
+++ src/python/srf/graph/reconstruction/local_reconstruction.py
@@ -1,4 +1,4 @@
-from dxl.learn.core import Graph, NoOp, SubgraphMakerTable
+from dxl.learn.core import Graph, NoOp, ThisSession
 
 from .master import MasterGraph
 from .worker import WorkerGraph
@@ -25,47 +25,36 @@ class LocalReconstructionGraph(Graph):
             MASTER = 'master'
             WORKER = 'worker'
 
-    def __init__(self, info, master_data_loader, worker_data_loader, *, config=None, nb_iteration=10):
-        self._master_data_loader = master_data_loader
-        self._worker_data_loader = worker_data_loader
-        config = self._parse_input_config(config, {
-            self.KEYS.CONFIG.NB_ITERATIONS: nb_iteration,
-        })
-        super().__init__(info, config=config)
+    def __init__(self, info, master_graph, worker_graph, *, nb_iteration=10):
+        super().__init__(info,
+                         graphs={
+                             self.KEYS.GRAPH.MASTER: master_graph,
+                             self.KEYS.GRAPH.WORKER: worker_graph
+                         },
+                         config={
+                             self.KEYS.CONFIG.NB_ITERATIONS: nb_iteration,
+                         })
 
-    def kernel(self):
+    def kernel(self, inputs=None):
         KS, KT = self.KEYS.GRAPH, self.KEYS.TENSOR
-        # m = self.graphs[KS.MASTER] = MasterGraph(self.info.child_scope(KS.MASTER),
-        #                                             loader=self._master_data_loader, nb_workers=1)
-        # self.tensors[KT.X] = m.tensor(KT.X)
-        # w = self.graphs[KS.WORKER] = WorkerGraph(self.info.child_scope(KS.WORKER), m.tensor(
-        #     KT.X), m.tensor(m.KEYS.TENSOR.BUFFER)[0], loader=self._worker_data_loader, recon_step_cls=ReconStep)
-        # with tf.control_dependencies([m.tensor(m.KEYS.TENSOR.INIT).data, w.tensor(w.KEYS.TENSOR.INIT).data]):
-        #     self.tensors[KT.INIT] = NoOp()
-        # self.tensors[KT.RECONSTRUCTION_STEP] = w.tensor(w.KEYS.TENSOR.UPDATE)
-        # self.tensors[KT.UPDATE] = m.tensor(m.KEYS.TENSOR.UPDATE)
-        m = self.graphs[KS.MASTER] = MasterGraph(self.info.child_scope(KS.MASTER),
-                                                 loader=self._master_data_loader, nb_workers=1)
-        self.tensors[KT.X] = m.get_or_create_tensor(KT.X)
-        w = self.graphs[KS.WORKER] = WorkerGraph(self.info.child_scope(KS.WORKER), m.get_or_create_tensor(
-            KT.X), m.get_or_create_tensor(m.KEYS.TENSOR.BUFFER)[0], loader=self._worker_data_loader, recon_step_cls=ReconStep)
-        with tf.control_dependencies([m.get_or_create_tensor(m.KEYS.TENSOR.INIT).data, w.get_or_create_tensor(w.KEYS.TENSOR.INIT).data]):
+        m = self.graphs[KS.MASTER]
+        m.make()
+        self.tensors[KT.X] = m.tensors[KT.X]
+        w = self.graphs[KS.WORKER]
+        w.tensors[KT.X] = m.tensors[KT.X]
+        w.tensors[w.KEYS.TENSOR.TARGET] = m.tensors[m.KEYS.TENSOR.BUFFER][0]
+        w.make()
+        with tf.control_dependencies([m.tensors[m.KEYS.TENSOR.INIT].data,
+                                      w.tensors[w.KEYS.TENSOR.INIT].data]):
             self.tensors[KT.INIT] = NoOp()
-        self.tensors[KT.RECONSTRUCTION_STEP] = w.get_or_create_tensor(
-            w.KEYS.TENSOR.UPDATE)
-        self.tensors[KT.UPDATE] = m.get_or_create_tensor(m.KEYS.TENSOR.UPDATE)
+        self.tensors[KT.RECONSTRUCTION_STEP] = w.tensors[w.KEYS.TENSOR.UPDATE]
+        self.tensors[KT.UPDATE] = m.tensors[m.KEYS.TENSOR.UPDATE]
 
-    def run(self, sess):
+    def run(self, sess=None):
         KT, KC = self.KEYS.TENSOR, self.KEYS.CONFIG
-        # sess.run(self.tensor(KT.INIT))
-        # for i in tqdm(range(self.config(KC.NB_ITERATIONS))):
-        #     sess.run(self.tensor(KT.RECONSTRUCTION_STEP))
-        #     sess.run(self.tensor(KT.UPDATE))
-        #     x = sess.run(self.tensor(KT.X))
-        #     np.save('recon_{}.npy'.format(i), x)
-        sess.run(self.get_or_create_tensor(KT.INIT))
+        sess.run(self.tensors[KT.INIT])
         for i in tqdm(range(self.config(KC.NB_ITERATIONS))):
-            sess.run(self.get_or_create_tensor(KT.RECONSTRUCTION_STEP))
-            sess.run(self.get_or_create_tensor(KT.UPDATE))
-            x = sess.run(self.get_or_create_tensor(KT.X))
+            sess.run(self.tensors[KT.RECONSTRUCTION_STEP])
+            sess.run(self.tensors[KT.UPDATE])
+            x = sess.run(self.tensors[KT.X])
             np.save('recon_{}.npy'.format(i), x)
