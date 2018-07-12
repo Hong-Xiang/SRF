cd349bfc4fcce29674e2f2473254f2d6ea2942b2
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-07-12 16:08:58 +0800

    clean the example file

--- examples/effmap/local_ring.py
+++ examples/effmap/local_ring.py
@@ -1,60 +1,65 @@
 import time
-import tensorflow as tf 
+import tensorflow as tf
+import numpy as np
 from tqdm import tqdm
 
 from srf.scanner.pet.pet import CylindricalPET
 from srf.scanner.pet.geometry import RingGeometry
-from srf.scanner.pet.spec import TOF 
-from srf.scanner.pet.block import Block
-from srf.preprocess import preprocess
+from srf.scanner.pet.spec import TOF
+from srf.scanner.pet.block import Block,RingBlock
+from srf.preprocess.function.on_tor_lors import map_process
 
-from srf.graph.reconstruction.ring_effciency_map import RingEfficiencyMap
+from srf.graph.reconstruction.ring_efficiency_map import RingEfficiencyMap
 
-from srf.model.backprojection import BackProjectioTOR
-from srf.model.map_step import MapStep 
-from srf.physics.tor_map import ToRMapModel
+from srf.model.backprojection import BackProjectionToR
+from srf.model.map_step import MapStep
+from srf.physics import ToRMapModel
+from srf.preprocess.merge_map import merge_effmap
+from dxl.learn.core import Session
 
+def compute(lors, grid, center, size, kernel_width):
 
-def compute():
+    physics_model = ToRMapModel('map_model', kernel_width=kernel_width)
+    backprojection_model = BackProjectionToR(projection_model=physics_model)
+    map_step = MapStep('compute/effmap', backprojection=backprojection_model)
 
-    model = ToR    
-
-    config = tf.ConfigProto
-    config.gpu_option.allow_growth = True
-    t = RingEfficiencyMap()
-    result = t.tensors[t.KT.TENSOR.RESULT]
+    t = RingEfficiencyMap('effmap', compute_graph=map_step,
+                          lors=lors, grid=grid,
+                          center=center, size=size)
     t.make()
-    with tf.Session(config=config) as sess:
-        result = sess.run(result)
-    tf.reset_default_graph()
+    with Session() as sess:
+        result = t.run()
+        print(result[result>0])
+    sess.reset()
     return result
+
 
 def get_mct_config():
     """
     Retrun a dict that describes the geometry of mCT scanner.
     """
     config = {
-        "modality": "PET",
-        "name": "mCT",
-        "ring": {
+            "modality":"PET",
+            "name": "mCT",
+            "ring":{
                 "inner_radius": 424.5,
                 "outer_radius": 444.5,
                 "axial_length": 220.0,
-                "nb_rings": 4,
+                "nb_rings": 104,
                 "nb_blocks_per_ring": 48,
-                "gap": 4.0
-        },
-        "block": {
-            "grid": [1, 13, 13],
-            "size": [20.0, 52.0, 52.0],
-            "interval": [0.0, 0.0, 0.0]
-        },
-        "tof": {
-            "resolution": 530,
-            "bin": 40
-        }
+                "gap": 0.0
+            },
+            "block":{
+                "grid":[1, 13, 1],
+                "size":[20.0, 53.3, 2.05],
+                "interval": [0.0, 0.0, 0.0]
+            },
+            "tof":{
+                "resolution":530,
+                "bin": 40
+            }
     }
-    return config        
+    return config
 
 
 def make_scanner():
@@ -63,39 +68,40 @@ def make_scanner():
     """
     config = get_mct_config()
     ring = RingGeometry(config['ring'])
-    block = Block(block_size = config['block']['size'],
-                    grid = config['block']['grid'])
+    block = Block(block_size=config['block']['size'],
+                  grid=config['block']['grid'])
     name = config['name']
     # modality = config['modality']
-    tof = TOF(res = config['tof']['resolution'], bin = config['tof']['bin'])
-    return CylindricalPET(name, ring, block, tof)    
-
+    tof = TOF(res=config['tof']['resolution'], bin=config['tof']['bin'])
+    return CylindricalPET(name, ring, block, tof)
 
 
 def main():
-    kernel_width = 7.76
-    grid = [90, 90, 24]
+    grid = [128, 128, 104]
     center = [0.0, 0.0, 0.0]
-    size = [150.3, 150.3, 40.08]
+    size = [262.4, 262.4, 213.2]
+    kernel_width = 7.76
     rpet = make_scanner()
-
     r1 = rpet.rings[0]
-    
-    for ir in tqdm(range(0,  rpet.nb_rings)):
+
+    for ir in tqdm(range(0, rpet.nb_rings)):
         print("start to compute the {} th map.".format(ir))
         r2 = rpet.rings[ir]
         lors = rpet.make_ring_pairs_lors(r1, r2)
-        lors = preprocess(lors)
-        
-        effmap = compute(lors, )
+        # np.save('debug_lors.npy',lors)
+        lors = map_process(lors)
+
+        from srf.preprocess.function.on_tor_lors import Axis as AXIS
+        # print("preprocessed lors number is:",lors[AXIS.x].shape[0]+lors[AXIS.y].shape[0]+lors[AXIS.z].shape[0])
+        # np.save("processed_lors.npy", lors[AXIS.x])
+        # print(lors[AXIS.y][0,:])
+        # print(lors[AXIS.z][0,:])
+        result = compute(lors, grid, center, size, kernel_width)
+
         np.save('effmap_{}.npy'.format(ir), result)
-
-        
-
-
-
-
-
-
-
     
+    merge_effmap(0, rpet.nb_rings, rpet.nb_rings, 1, './')
+
+
+if __name__ == '__main__':
+    main()
