90efe9f357362bb8e041674671c3579a792b0214
HongXiang <hx.hongxiang@gmail.com>
2018-07-23 13:30:51 +0800

    id_sinogram test passed

--- src/python/srf/external/stir/function/_listmode2sinogram.py
+++ src/python/srf/external/stir/function/_listmode2sinogram.py
@@ -1,43 +1,48 @@
 from dxl.data import List, Pair
 from dxl.data.tensor import Point
 from dxl.function import x
-from srf.data import Event, LoR, ListModeData, PETSinogram3D, PETCylindricalScanner
+from srf.data import DetectorIdEvent, LoR, ListModeData, PETSinogram3D, PETCylindricalScanner
 import numpy as np
+from functools import partial
+
+__all__ = ['listmode2sinogram']
 
 
 def listmode2sinogram(scanner: PETCylindricalScanner, listmode_data: ListModeData) -> PETSinogram3D:
-    return accumulating2sinogram(scanner, listmode_data.fmap(rework_indices))
+    data_with_fixed_ids = listmode_data.fmap(partial(rework_indices, scanner))
+    return accumulating2sinogram(scanner, data_with_fixed_ids)
 
 
 def rework_indices(scanner: PETCylindricalScanner, lor: LoR):
     return fix_ring_index(lor.fmap2(lambda e: fix_crystal_id(scanner, e)),
-                          scanner.nb_detectors)
+                          scanner.nb_detectors_per_ring)
 
 
-def fix_crystal_id(scanner, event):
-    fixed_id = event.id_crystal + scanner.nb_detectors // 4 % scanner.nb_detectors
+def fix_crystal_id(scanner, event: DetectorIdEvent)->int:
+    fixed_id = ((event.id_crystal + scanner.nb_detectors_per_ring // 4)
+                % scanner.nb_detectors_per_ring)
     return event.replace(id_crystal=fixed_id)
 
 
-def fix_ring_index(lor, nb_detectors):
-    crystal_centers = lor.fmap2(
+def fix_ring_index(lor: LoR, nb_detectors: int) -> LoR:
+    direction = lor.fmap2(
         lambda e: center_of_crystal(e.id_crystal, nb_detectors))
-    ring_ids, crystal_ids = lor.fmap(x.id_ring), lor.fmap(x.id_crystal)
-    if is_need_swap_crystal_id(crystal_centers):
+    ring_ids, crystal_ids = lor.fmap2(x.id_ring), lor.fmap2(x.id_crystal)
+    if is_need_swap_ring_id(direction):
         ring_ids = ring_ids.flip()
-    if is_need_swap_rind_id(crystal_centers):
+    if is_need_swap_crystal_id(direction):
         crystal_ids = crystal_ids.flip()
-    return LoR(Event(ring_ids.fst, crystal_ids.fst),
-               Event(ring_ids.snd, crystal_ids.snd))
+    return LoR(DetectorIdEvent(ring_ids.fst, None, crystal_ids.fst),
+               DetectorIdEvent(ring_ids.snd, None, crystal_ids.snd))
 
 
-def center_of_crystal(crystal_id, nb_detectors):
+def center_of_crystal(crystal_id: int, nb_detectors: int) -> Point:
     x = np.sin((0.5 + crystal_id) * (2 * np.pi) / nb_detectors)
     y = np.cos((0.5 + crystal_id) * (2 * np.pi) / nb_detectors)
     return Point([x, y])
 
 
-def is_need_swap_rind_id(ps: Pair[Point, Point]) -> bool:
+def is_need_swap_ring_id(ps: Pair[Point, Point]) -> bool:
     if ps.fst.x > ps.snd.x:
         return True
     if ps.fst.x == ps.snd.x and ps.fst.y < ps.snd.y:
@@ -53,44 +58,53 @@ def is_need_swap_crystal_id(ps: Pair[Point, Point]) -> bool:
     return False
 
 
-def accumulating2sinogram(scanner, lors: ListModeData):
+def accumulating2sinogram(scanner, lors: ListModeData) -> PETSinogram3D:
     nb_views_, nb_sinograms_ = nb_views(scanner), nb_sinograms(scanner)
     result = np.zeros([nb_views_, nb_views_, nb_sinograms_])
+    print(nb_views_)
     for lor in lors:
-        ring_ids = lors.fmap2(x.id_ring)
+        ring_ids = lor.fmap2(x.id_ring)
         id_bin_ = id_bin(scanner, ring_ids)
-        if id_bin_ > 0 and id_bin_ < nb_views_:
+        print(id_sinogram(scanner, ring_ids),
+              id_bin_, id_view(scanner, ring_ids))
+        if id_bin_ >= 0 and id_bin_ < nb_views_:
             result[id_sinogram(scanner, ring_ids), id_bin_,
                    id_view(scanner, ring_ids)] += 1
     return PETSinogram3D(result)
 
 
-def nb_views(scanner):
-    return scanner.nb_detectors // 2
+def nb_views(scanner) -> int:
+    return scanner.nb_detectors_per_ring // 2
 
 
-def nb_sinograms(scanner):
+def nb_sinograms(scanner) -> int:
     return scanner.nb_rings * scanner.nb_rings
 
 
-def id_sinogram(scanner, ring_ids):
+def id_sinogram(scanner, ring_ids: Pair[int, int]) -> int:
     delta_z = ring_ids.snd - ring_ids.fst
-    result = (ring_ids.fst + ring_ids.snd - abs(delta_z)) // 2 + \
-        (scanner.nb_ring if delta_z != 0 else 0)
+    result = (ring_ids.fst + ring_ids.snd - abs(delta_z)) / 2.0
+    if delta_z != 0:
+        result += scanner.nb_rings
     for i in range(1, abs(delta_z)):
         result += 2 * (scanner.nb_rings - i)
     if(delta_z < 0):
-        result += scanner.nb_ring - abs(delta_z)
-    return result
+        result += (scanner.nb_rings - abs(delta_z))
+    return int(result)
 
 
-def id_view(scanner, ring_ids):
-    return (int(sum(ring_ids) + scanner.nb_detectors // 2 + 1) / 2) % scanner.nb_detectors // 2
+def id_view(scanner, ring_ids: Pair[int, int]) -> int:
+    half_dct = scanner.nb_detectors_per_ring // 2
+    return (ring_ids.fst + ring_ids.snd + half_dct + 1) // 2 % half_dct
 
 
-def id_bin(scanner, ring_ids):
+def id_bin(scanner, ring_ids: Pair[int, int]) -> int:
+    id_view_ = id_view(scanner, ring_ids)
+
     def diff(id_):
-        return min(abs(id_ - id_view), abs(id_ - (id_view + scanner.nb_detectors)))
+        v0 = id_ - id_view_
+        v1 = id_ - (id_view_ + scanner.nb_detectors_per_ring)
+        return v0 if abs(v0) < abs(v1) else v1
     diffs = ring_ids.fmap(diff)
     if (abs(diffs.fst) < abs(diffs.snd)):
         sigma = ring_ids.fst - ring_ids.snd
@@ -98,5 +112,6 @@ def id_bin(scanner, ring_ids):
         sigma = ring_ids.snd - ring_ids.fst
 
     if (sigma < 0):
-        sigma += scanner.nb_detectors
-    return sigma + (nb_views) / 2 - scanner.nb_detectors / 2
+        sigma += scanner.nb_detectors_per_ring
+    result = int(sigma + (nb_views(scanner)) / 2 - scanner.nb_detectors_per_ring / 2)
+    return result
--- tests/python/external/stir/function/test_listmode2sinogram.py
+++ tests/python/external/stir/function/test_listmode2sinogram.py
@@ -1,12 +1,16 @@
 import pytest
 import numpy as np
 from srf.data import ListModeData, PETSinogram3D, PETCylindricalScanner, Block
-from srf.external.stir.function import ndarray2listmode, listmode2sinogram
+from srf.external.stir.function import ndarray2listmode, listmode2sinogram, position2detectorid
+from srf.external.stir.function._listmode2sinogram import rework_indices, id_sinogram, id_view, id_bin
+from functools import partial
+from dxl.function import x
+from dxl.data import List
 
 
 @pytest.fixture
-def listmode_data(l2sdata):
-    return ndarray2listmode(l2sdata['input'])
+def listmode_data(scanner, l2sdata):
+    return ndarray2listmode(l2sdata['input']).fmap(lambda p: p.fmap2(partial(position2detectorid, scanner)))
 
 
 @pytest.fixture
@@ -14,6 +18,35 @@ def l2s_expected_result(l2sdata):
     return PETSinogram3D(l2sdata['result'])
 
 
-@pytest.mark.skip('NIY')
-def test_listmode_to_sinogram(scanner, listmode_data):
+def test_listmode_to_sinogram(scanner, listmode_data, l2s_expected_result):
     sinogram = listmode2sinogram(scanner, listmode_data)
+    print(np.sum(sinogram.data))
+    assert np.sum(sinogram.data[:, :, 3]) == 2
+    assert sinogram == l2s_expected_result
+
+
+@pytest.fixture
+def ring_ids(scanner, listmode_data):
+    lors = listmode_data.fmap(partial(rework_indices, scanner))
+    return lors.fmap(lambda l: l.fmap2(lambda e: e.id_ring))
+
+
+@pytest.fixture
+def id_sinogram_matlab(stir_data_root):
+    data = np.load(stir_data_root / 'sinogramids.npz')
+    return {k: List((v - 1).tolist()) for k, v in data.items()}
+
+
+def test_id_sinogram(scanner, ring_ids, stir_data_root, id_sinogram_matlab):
+    result = ring_ids.fmap(partial(id_sinogram, scanner))
+    assert result == id_sinogram_matlab['id_sinogram']
+
+
+def test_id_bin(scanner, ring_ids, stir_data_root, id_sinogram_matlab):
+    result = ring_ids.fmap(partial(id_bin, scanner))
+    assert result == id_sinogram_matlab['id_bin']
+
+
+def test_id_view(scanner, ring_ids, stir_data_root, id_sinogram_matlab):
+    result = ring_ids.fmap(partial(id_view, scanner))
+    assert result == id_sinogram_matlab['id_view']
