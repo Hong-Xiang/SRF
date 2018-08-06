9baf61556c6ffee2fb7b92d6bfd207e80f8a625b
chengaoyu <chengaoyu@sjtu.edu.cn>
2018-08-06 22:25:47 +0800

    adjust the projection and backprojection entry and model

--- src/python/srf/physics/__init__.py
+++ src/python/srf/physics/__init__.py
@@ -1,3 +1,6 @@
-from .tor import ToRModel
-from .tor_map import ToRMapModel
-from .siddon import SiddonModel
+# from .tor import ToRModel
+# from .tor_map import ToRMapModel
+# from .siddon import SiddonModel
+
+from .complete_lors import CompleteLorsModel
+from .split_lors import SplitLorsModel
