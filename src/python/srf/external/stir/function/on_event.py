de52973cf235a581eccfc2cd31d05d19cbf0d39a
twj2417 <twj2417@163.com>
2018-09-22 17:00:21 +0800

    modified sinogram

--- src/python/srf/external/stir/function/on_event.py
+++ src/python/srf/external/stir/function/on_event.py
@@ -1,5 +1,6 @@
 from srf.data import PositionEvent, DetectorIdEvent, PETCylindricalScanner
 import numpy as np
+import math
 from doufo.tensor import norm,Vector
 
 __all__ = ['position2detectorid']
@@ -12,10 +13,10 @@ def position2detectorid(scanner: PETCylindricalScanner, event: PositionEvent) ->
 
 
 def block_id(scanner, position):
-    return int(theta(scanner, position) / block_angle(scanner) + 0.5) % scanner.nb_blocks_per_ring
+    return int(theta(position) / block_angle(scanner) + 0.5) % scanner.nb_blocks_per_ring
 
 
-def theta(scanner, position):
+def theta(position):
     result = np.math.acos(Vector(position).x / norm(p_xy(position)))
     if Vector(position).y < 0:
         result = 2 * np.pi - result
@@ -34,23 +35,33 @@ def ring_id(scanner, position):
     return int((Vector(position).z / scanner.axial_length + 0.5) * scanner.nb_rings*scanner.blocks[0].grid[2])
 
 
+# def crystal_id(scanner, position):
+#     bid = block_id(scanner, position)
+#     shift = ((p_xy(position) - center(scanner, bid))) @ normal(scanner, bid)
+#     result = int((shift + scanner.block_width / 2) / scanner.crystal_width)
+#     # FIXME handle of odd nb_detectors_per_block
+#     previous = bid * scanner.nb_detectors_per_block
+#     return result + previous
+
 def crystal_id(scanner, position):
-    bid = block_id(scanner, position)
-    shift = ((p_xy(position) - center(scanner, bid))) @ normal(scanner, bid)
-    result = int((shift + scanner.block_width / 2) / scanner.crystal_width)
-    # FIXME handle of odd nb_detectors_per_block
-    previous = bid * scanner.nb_detectors_per_block
-    return result + previous
+    fi = math.degrees(theta(position))
+    fixed_fi = (fi+180 / scanner.nb_blocks_per_ring) % 360
+    return int(fixed_fi / 360 * scanner.nb_detectors_per_ring)-int(scanner.nb_detectors_per_block/2)
 
 
-def direction(scanner, block_id_):
-    phi = block_id_ * block_angle(scanner)
-    return np.array([np.math.cos(phi), np.math.sin(phi)])
+def normal(p:Vector):
+    len = p.x*p.x+p.y*p.y
+    return len ** 0.5
 
 
-def normal(scanner, block_id_):
-    d = direction(scanner, block_id_)
-    return np.array([-d[1], d[0]])
+# def direction(scanner, block_id_):
+#     phi = block_id_ * block_angle(scanner)
+#     return np.array([np.math.cos(phi), np.math.sin(phi)])
+
+
+# def normal(scanner, block_id_):
+#     d = direction(scanner, block_id_)
+#     return np.array([-d[1], d[0]])
 
 
 def center(scanner, block_id_):
