46157ec87356fb979ea84fdac989b8b8832e39b2
hongjiang <hongjiangdong@outlook.com>
2018-10-16 21:39:22 +0800

    update install requires

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
+
 setup(name='SRF',
-      version='0.2.1',
+      version='0.2.2',
       description='Scalable Reconstruction Framework.',
       url='https://github.com/tech-pi/SRF',
       author='Hong Xiang',
@@ -8,11 +9,23 @@ setup(name='SRF',
       license='Apache',
       packages=find_packages('src/python'),
       package_dir={'': 'src/python'},
-      install_requires=['dxl-learn>=0.2.0',
-                        'dxl-core>=0.1.6',
-                        'tensorflow',
-                        'doufo>=0.0.3',
-                        'dxl-shape>=0.1.1'],
+      install_requires=[
+          'dxl-learn==0.2.1',
+          'dxl-core==0.1.7',
+          'doufo==0.0.4',
+          'dxl-shape==0.1.2',
+          'jfs==0.1.3',
+          'scipy',
+          'matplotlib',
+          'typing',
+          'h5py',
+          'click',
+          'jinja2',
+          'pathlib',
+          'numpy',
+          'tqdm',
+          'tensorflow'
+      ],
       entry_points="""
             [console_scripts]
             srf=srf.api.cli.main:srf
